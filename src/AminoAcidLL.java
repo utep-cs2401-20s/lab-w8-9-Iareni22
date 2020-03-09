558a5e82c2e373cc31dbce0ff8e1e8d1faf7e2d1
iareni22 <ivaneins@hotmail.com>
2020-03-09 14:48:37 -0600

    Created constructor

--- src/AminoAcidLL.java
+++ src/AminoAcidLL.java
@@ -14,7 +14,11 @@ class AminoAcidLL{
    * pair and increments the codon counter for that codon.
    * NOTE: Does not check for repeats!! */
   AminoAcidLL(String inCodon){
-  
+
+    aminoAcid = AminoAcidResources.getAminoAcidFromCodon(inCodon);
+    codons = AminoAcidResources.getCodonListForAminoAcid(aminoAcid);
+    counts = new int[codons.length];
+
   }
 
   /********************************************************************************************/
@@ -91,6 +95,7 @@ class AminoAcidLL{
   /********************************************************************************************/
   /* Static method for generating a linked list from an RNA sequence */
   public static AminoAcidLL createFromRNASequence(String inSequence){
+    
     return null;
   }
 
